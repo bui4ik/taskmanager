076732755f3bf8bdde8c2bdb6ba6af640a323f22
Max Buinevich <maksym.b@polontech.biz>
2020-10-25 15:54:33 +0300

    Add polling

--- src/layouts/DefaultLayout/DefaultLayout.js
+++ src/layouts/DefaultLayout/DefaultLayout.js
@@ -1,14 +1,21 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
 import { Layout } from 'antd'
+import { getProcessesPending } from 'store/process/slice'
 import { StyledLayout, StyledContent } from './styles'
+import useInterval from './useInterval'
 
 const { Header } = Layout
 
-const DefaultLayout = ({ children }) => (
-  <StyledLayout>
-    <Header />
-    <StyledContent>{children}</StyledContent>
-  </StyledLayout>
-)
+const DefaultLayout = ({ children }) => {
+  const dispatch = useDispatch()
+  useInterval(() => dispatch(getProcessesPending()), 600000)
+  return (
+    <StyledLayout>
+      <Header />
+      <StyledContent>{children}</StyledContent>
+    </StyledLayout>
+  )
+}
 
 export default DefaultLayout
